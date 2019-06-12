ac37b91d966426865372305ae53f550db0348748
Aliaksei Maiseyeu <jetfire6@gmail.com>
2019-06-12 13:45:55 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ locals {
 data "aws_partition" "current" {}
 
 resource "aws_security_group" "allow_in80_in443_outALL" {
-  count       = "${ var.alb_custom_security_group == "None" ? 0 : 1 }"
+  count       = "${ var.alb_custom_security_group == "None" ? 1 : 0 }"
   name        = "allow-in_80-in_443-out_ALL-${var.project}-${var.environment}"
   description = "Allow inbound traffic on ports 80 and 443"
   vpc_id      = "${var.vpc_id}"
